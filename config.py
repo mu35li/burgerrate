b4625a0c91538886ec35349d53ace23dab778bab
Julian Deinert <j.deinert@gmx.net>
2016-07-12 20:19:59 +0200

    Change location of db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = True
 SECRET_KEY = "foobar"
-SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/burgerrate-development.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:////srv/burgerrate/tmp/burgerrate-development.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- runburgerrate.py
+++ runburgerrate.py
@@ -1,2 +1,2 @@
 from burgerrate import app
-app.run(debug=True, host='127.0.0.1')
+app.run(debug=False, host='0.0.0.0')
