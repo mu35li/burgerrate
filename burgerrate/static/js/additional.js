b551794f7f0f05b5dedaca7139d068b6e62e3aeb
Julian Deinert <j.deinert@gmx.net>
2016-07-30 18:42:04 +0200

    Add wizzard to addrating page

--- burgerrate/static/js/additional.js
+++ burgerrate/static/js/additional.js
@@ -45,8 +45,34 @@ $(document).ready(function (){
 		replaceBurger($(this).text());
 		$(this).closest(".hint").addClass("hidden");
 	});
+
+	$(".ui.button.next").on("click", function(event){
+		event.preventDefault();
+		var nextPage = $(this).parents(".wizzardpage").next(".wizzardpage");
+		$(this).parents(".wizzardpage").addClass("hidden");
+		nextPage.removeClass("hidden");
+		if (nextPage.attr("id") === "final") {
+			$(".step").removeClass("active").addClass("completed");
+		}else{
+			updateStep(nextPage.data("step"));
+		}
+	});
+
+	$(".ui.button.previous").on("click", function(event){
+		event.preventDefault();
+		$(this).parents(".wizzardpage").addClass("hidden");
+		$(this).parents(".wizzardpage").prev(".wizzardpage").removeClass("hidden");
+		updateStep($(this).parents(".wizzardpage").prev(".wizzardpage").data("step"));
+	});
 });
 
 function replaceBurger(burgerName) {
 	$("#burgerName").val(burgerName);
+}
+
+function updateStep(stepId) {
+	$(".step").removeClass("active");
+	$(".step[data-step='"+stepId+"']").prevAll().addClass("completed");
+	$(".step[data-step='"+stepId+"']").removeClass("completed");
+	$(".step[data-step='"+stepId+"']").addClass("active");
 }
