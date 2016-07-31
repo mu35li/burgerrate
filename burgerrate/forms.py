2d108401f1942abfb6c204212ea1154e238a2136
Julian Deinert <j.deinert@gmx.net>
2016-07-31 20:57:17 +0200

    Add basic form validation to addRating form

--- burgerrate/forms.py
+++ burgerrate/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import Form
-from wtforms import StringField, SelectField
+from wtforms import StringField, SelectField, validators
 
 class RatingForm(Form):
     ratingNumbers = [('0', '0'), ('1', '1'),('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9'),('10', '10')]
     offerRating = SelectField('Offer', choices = ratingNumbers)
     waiterRating = SelectField('Waiter', choices = ratingNumbers)
     athmosphereRating = SelectField('Athmosphere', choices = ratingNumbers)
-    burgerName = StringField('Burger Name')
+    burgerName = StringField('Burger Name', [validators.DataRequired()])
     meatRating = SelectField('Meat', choices = ratingNumbers)
     sauceRating = SelectField('Sauce', choices = ratingNumbers)
     burgerQualityRating = SelectField('Quality', choices = ratingNumbers)
-    sideName = StringField('Side Name')    
+    sideName = StringField('Side Name', [validators.DataRequired()])    
     sidesQualityRating = SelectField('Quality', choices = ratingNumbers)
     sidesQuantityRating = SelectField('Quantity', choices = ratingNumbers)
 
