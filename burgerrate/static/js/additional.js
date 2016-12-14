7a455a08743a95df801a77b9adff730ccf16bc07
Julian Deinert <j.deinert@gmx.net>
2016-12-14 02:02:11 +0100

    Add some simple form validation

--- burgerrate/static/js/additional.js
+++ burgerrate/static/js/additional.js
@@ -7,76 +7,86 @@ $('.ui.rating')
 ;
 
 $('.ratingBar').progress({
-	total: 10,
-	showActivity: true,
-	label: 'ratio',
-	text: {
-		ratio: '{value} / {total}'
-	}
+    total: 10,
+    showActivity: true,
+    label: 'ratio',
+    text: {
+        ratio: '{value} / {total}'
+    }
 });
 
 $(document).ready(function (){
-	var restaurantId = $('#restaurantId').val();
+    var restaurantId = $('#restaurantId').val();
 
-	$('#burgerName').on('focusout', function() {
-		$.ajax({
-			context: $(this),
-			url: '/spellingHints/',
-			method: "POST",
-			data: {
-				name: $(this).val(),
-				mealType: "burger",
-				restaurantId: restaurantId
-			},
-			success: function(data) {
-				if (typeof data === "string") {
-					var messageBox = $(this).closest(".field").find(".hint"); 
-					messageBox.find('p').html("Sure you didn't mean <a href='' id='burgerReplaceLink'>"+data+"</button>");
-					messageBox.removeClass('hidden');
-				}else{
-					console.log(data);
-				}
-			}
-		});
-	});
+    $('#burgerName').on('focusout', function() {
+        $.ajax({
+            context: $(this),
+            url: '/spellingHints/',
+            method: "POST",
+            data: {
+                name: $(this).val(),
+                mealType: "burger",
+                restaurantId: restaurantId
+            },
+            success: function(data) {
+                if (typeof data === "string") {
+                    var messageBox = $(this).closest(".field").find(".hint"); 
+                    messageBox.find('p').html("Sure you didn't mean <a href='' id='burgerReplaceLink'>"+data+"</button>");
+                    messageBox.removeClass('hidden');
+                }else{
+                    console.log(data);
+                }
+            }
+        });
+    });
 
-	$("body").on("click", "#burgerReplaceLink", function(event){
-		event.preventDefault();
-		replaceBurger($(this).text());
-		$(this).closest(".hint").addClass("hidden");
-	});
+    $('.ui.form')
+    .form({
+        fields: {
+            burgerName : {
+                rules: [
+                {
+                    type: "empty",
+                    prompt: "Please enter the name of the burger you had."
+                }
+                ]
+            }
+        }
+    });
 
-	$(".ui.button.next").on("click", function(event){
-		event.preventDefault();
-		var nextPage = $(this).parents(".wizzardpage").next(".wizzardpage");
-		$(this).parents(".wizzardpage").addClass("hidden");
-		nextPage.removeClass("hidden");
-		if (nextPage.attr("id") === "final") {
-			$(".step").removeClass("active").addClass("completed");
-		}else{
-			updateStep(nextPage.data("step"));
-		}
-	});
+    $("body").on("click", "#burgerReplaceLink", function(event){
+        event.preventDefault();
+        replaceBurger($(this).text());
+        $(this).closest(".hint").addClass("hidden");
+    });
 
-	$(".ui.button.previous").on("click", function(event){
-		event.preventDefault();
-		$(this).parents(".wizzardpage").addClass("hidden");
-		$(this).parents(".wizzardpage").prev(".wizzardpage").removeClass("hidden");
-		updateStep($(this).parents(".wizzardpage").prev(".wizzardpage").data("step"));
-	});
+    $(".ui.button.next").on("click", function(event){
+        event.preventDefault();
+        var nextPage = $(this).parents(".wizzardpage").next(".wizzardpage");
+        $(this).parents(".wizzardpage").addClass("hidden");
+        nextPage.removeClass("hidden");
+        if (nextPage.attr("id") === "final") {
+            $(".step").removeClass("active").addClass("completed");
+        }else{
+            updateStep(nextPage.data("step"));
+        }
+    });
 
-	$(".addrating").on("click", function() {
-		location.href=$(this).data("url");
-	});
+    $(".ui.button.previous").on("click", function(event){
+        event.preventDefault();
+        $(this).parents(".wizzardpage").addClass("hidden");
+        $(this).parents(".wizzardpage").prev(".wizzardpage").removeClass("hidden");
+        updateStep($(this).parents(".wizzardpage").prev(".wizzardpage").data("step"));
+    });
 });
 
 function replaceBurger(burgerName) {
-	$("#burgerName").val(burgerName);
+    $("#burgerName").val(burgerName);
 }
 
 function updateStep(stepId) {
-	$(".step").removeClass("active");
-	$(".step[data-step='"+stepId+"']").prevAll().addClass("completed");
-	$(".step[data-step='"+stepId+"']").removeClass("completed");
-	$(".step[data-step='"+stepId+"']").addClass("active");
+    $(".step").removeClass("active");
+    $(".step[data-step='"+stepId+"']").prevAll().addClass("completed");
+    $(".step[data-step='"+stepId+"']").removeClass("completed");
+    $(".step[data-step='"+stepId+"']").addClass("active");
 }
